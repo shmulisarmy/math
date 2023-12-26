a09293773e5cce8687eca1a8ef6b000302dadbcb
shmulisarmy <shmulikeller@gmail.com>
2023-12-26 00:14:46 -0500

    reconfigured paths

--- main.py
+++ main.py
@@ -3,15 +3,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-@app.route('/')
-def home():
-    return render_template('index.html')
+
 @app.route('/algebra')
 def algebra():
     return render_template('algebra.html')
 @app.route('/multiplication')
 def multiplication():
     return render_template('multiplication.html')
+
+@app.route('/')
+def home():
+    return render_template('index.html')
 
 
 
